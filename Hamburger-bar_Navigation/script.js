29b74ed806c46b7e13a75d4b05da852aa2f20c06
Mazuros2 <s24440@pjwstk.edu.pl>
2024-02-20 12:56:42 +0100

    Final version

---
+++ Hamburger-bar_Navigation/script.js
@@ -0,0 +1,11 @@
+function showSidebar() 
+{
+    const sidebar = document.querySelector('.hamburger-bar');
+    sidebar.style.display = 'flex';
+}
+
+function hideSidebar() 
+{
+    const sidebar = document.querySelector('.hamburger-bar');
+    sidebar.style.display = 'none';
+}
